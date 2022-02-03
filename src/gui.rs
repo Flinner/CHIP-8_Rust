22b3f8066a2b9e11bd872c025ec2db79122cff57
Flinner <flinner.hlqn@aleeas.com>
2022-02-03 22:06:12 +0300

    feat: draw_from_mem with correct wrapping

--- src/gui.rs
+++ src/gui.rs
@@ -18,18 +18,34 @@ pub fn clear(d: &mut RaylibDrawHandle) {
     d.clear_background(Color::WHITE);
 }
 
-pub fn draw_from_mem(d: &mut RaylibDrawHandle, x: u8, y: u8, n: &[u8]) {
-    todo!()
+/// Pixels here should be between `PIXEL_{HEIGHT,WIDTH}` or it will wrap
+pub fn draw_from_mem(d: &mut RaylibDrawHandle, x: u8, y: u8, rows: &[u8]) {
+    // pixels can wrap
+    let x = x as usize % DISPLAY_WIDTH;
+    let y = y as usize % DISPLAY_HEIGHT;
+
+    // reg[0xF]
+    for row in rows {
+        for (ix, bit) in binary_iter(*row).enumerate() {
+            match bit {
+                0 => draw_pixel(d, (x + ix) as u8, y as u8, Color::BLACK),
+                1 => draw_pixel(d, (x + ix) as u8, y as u8, Color::WHITE),
+                _ => todo!(),
+            }
+        }
+    }
 }
 
-pub fn draw_pixel(d: &mut RaylibDrawHandle, x: u8, y: u8, n: u8) {
+// iterate over bits
+fn binary_iter(n: u8) -> impl Iterator<Item = u8> {
+    (1..=8).map(move |i| (n << (8 - i)) >> 7)
+}
+
+/// Pixels greater than PIXEL_{HEIGHT,WIDTH} will not be drawn!
+pub fn draw_pixel(d: &mut RaylibDrawHandle, x: u8, y: u8, color: Color) {
     let x = x as i32 * PIXEL_WIDTH;
     let y = y as i32 * PIXEL_HEIGHT;
-    // let x = (x as usize % DISPLAY_WIDTH) as i32 * PIXEL_WIDTH;
-    // let y = (y as usize % DISPLAY_HEIGHT) as i32 * PIXEL_HEIGHT;
 
-    // if black => white
-    // if white => black
     d.begin_blend_mode(BlendMode::BLEND_SUBTRACT_COLORS) // invert colors in subtraction mode
-        .draw_rectangle(x, y, PIXEL_WIDTH, PIXEL_HEIGHT, Color::WHITE); // white is used to invert
+        .draw_rectangle(x, y, PIXEL_WIDTH, PIXEL_HEIGHT, color); // white is used to invert
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,13 @@ use chip_8::gui;
 use raylib::prelude::*;
 
 fn main() {
-    // 10x10 pixels = pixel
     let (mut rl, thread) = raylib::init().size(640, 320).title("Hello, World").build();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
-        gui::draw_pixel(&mut d, 0, 9, 0);
-        gui::draw_pixel(&mut d, 10, 10, 0);
-        gui::draw_pixel(&mut d, 11, 11, 0);
-        // gui::draw_pixel(&mut d, 3, 3, &[1]);
-        // d.draw_text("Hello, world!", 12, 12, 20, Color::WHITE);
+        gui::draw_from_mem(&mut d, 62, 9, &[0x1]);
     }
     // let mut cpu = CPU::new();
 }
