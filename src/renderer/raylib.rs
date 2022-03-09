7159b85179ee3f3962b5088b27926ed5f6d2ec3d
Flinner <flinner.hlqn@aleeas.com>
2022-03-09 14:23:56 +0300

    feat: raylib draws screen

--- src/renderer/raylib.rs
+++ src/renderer/raylib.rs
@@ -4,12 +4,35 @@ use raylib::{RaylibHandle, RaylibThread};
 
 use crate::display::Display;
 
+use super::binary_iter;
+
 pub fn render(rl: &mut RaylibHandle, thread: &mut RaylibThread, display: &Display) {
     let mut d = rl.begin_drawing(thread);
     d.clear_background(Color::WHITE);
-    d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+
+    let (mut x, mut y) = (0, 0);
+    let scale = display.scale as i32;
+    for row in display.buffer {
+        for byte in row {
+            binary_iter(byte).for_each(|bit| {
+                d.draw_rectangle(x, y, scale, scale, bit_color(bit));
+                x += scale;
+            })
+        }
+        y += scale;
+        println!("y: {y}, x: {x}");
+        x = 0;
+    }
 }
 
 pub fn should_close(rl: &mut RaylibHandle) -> bool {
     rl.window_should_close()
+}
+
+fn bit_color(bit: u8) -> Color {
+    match bit {
+        0 => Color::BLACK,
+        1 => Color::WHITE,
+        _ => panic!(),
+    }
 }
