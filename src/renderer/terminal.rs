03c944b303d15be5c92e066c120331aedc189522
Flinner <flinner.hlqn@aleeas.com>
2022-03-15 23:14:03 +0300

    feat: `renderer::terminal` print display in one go

--- src/renderer/terminal.rs
+++ src/renderer/terminal.rs
@@ -8,19 +8,19 @@ use std::{thread, time};
 use super::keyboard::get_keycode;
 
 pub fn render(display: &Display) {
-    let mut fmted_row = String::with_capacity(64);
+    let mut fmted_grid = String::with_capacity(64);
 
     for row in display.buffer {
         for bit in row {
             match bit {
-                0 => fmted_row += "\x1b[1;30m██",
-                1 => fmted_row += "\x1b[1;37m██",
+                0 => fmted_grid += "\x1b[1;30m██",
+                1 => fmted_grid += "\x1b[1;37m██",
                 _ => panic!("Bit can only be 0 or 1"),
             }
         }
-        println!("{fmted_row}");
-        fmted_row.clear();
+        fmted_grid += "\n";
     }
+    println!("{fmted_grid}");
 }
 
 pub fn is_key_down(key: u8) -> bool {
